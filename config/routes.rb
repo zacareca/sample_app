46cf35fb2541fd43abf5c707092ce534013fa1a6
A M <rememberyourdreams@gmail.com>
2010-11-13 01:34:01 +0000

    Added a User factory and tests for the show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SampleApp::Application.routes.draw do
+
   resources :users
+
+  root :to => "pages#home"
 
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
@@ -56,7 +59,6 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
